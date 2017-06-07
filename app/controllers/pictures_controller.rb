e1bafff9deabfe78d92c43fca2e17f3cc04a7c1f
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-07 17:05:12 -0400

    Create form for new pic - works

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,5 +8,16 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
-  
+  def new
+
+    @picture = Picture.new
+    render :new
+  end
+
+  def create
+
+    render plain: "Received POST request to '/pictures' with the data URL: #{params.inspect} "
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
 get 'pictures' => 'pictures#index'
 
+post 'pictures' => 'pictures#create'
+
+get 'pictures/new' => 'pictures#new'
+
 get 'pictures/:id' => 'pictures#show'
 
 end
